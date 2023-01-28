e917b7708c7401cada0db1cbf36cf908b9e349f4
mohammadazzuz <112818173+mohammadazzuz@users.noreply.github.com>
2023-01-28 21:54:16 +0100

    add model

--- design/models.py
+++ design/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class MyData(models.Model):
+    title = models.CharField(max_length=100)
+    details = models.TextField(max_length=2000)
+    done = models.BooleanField(default=False)
+    created_data = models.DateTimeField()
