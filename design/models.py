9359cc9a6072fd6c6278a44bb30c69eb4c6d27b7
mohammadazzuz <112818173+mohammadazzuz@users.noreply.github.com>
2023-01-29 16:53:32 +0100

    sss

--- design/models.py
+++ design/models.py
@@ -5,5 +5,11 @@ from django.db import models
 class MyData(models.Model):
     title = models.CharField(max_length=100)
     details = models.TextField(max_length=2000)
+    image = models.ImageField(upload_to='portfolio/')
     done = models.BooleanField(default=False)
     created_data = models.DateTimeField()
+
+
+
+    def __str__(self):
+        return self.title 
--- design/views.py
+++ design/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import MyData
+from django.views.generic import ListView
+
+class PostList(ListView):
+    model = MyData
